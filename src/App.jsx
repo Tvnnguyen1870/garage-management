db25bcf61aaed5d7277b5036d74de1b83cbf3fae
nguyen <tovannguyen12354@gmail.com>
2023-10-23 23:33:00 +0700

    set up layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
-import { DatePicker } from 'antd';
+import { RouterProvider } from 'react-router-dom';
+import router from './router';
 
-function App() {
-  console.log(12123);
-  return <DatePicker />;
-}
-
+const App = () => {
+  return (
+    <div>
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+};
 export default App;
---
+++ src/components/Content.jsx
@@ -0,0 +1,22 @@
+import { theme } from 'antd';
+const Content = () => {
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
+  return (
+    <div>
+      <div
+        style={{
+          margin: '24px 16px',
+          padding: 24,
+          minHeight: 280,
+          background: colorBgContainer,
+        }}
+      >
+        Content
+      </div>
+    </div>
+  );
+};
+
+export default Content;
