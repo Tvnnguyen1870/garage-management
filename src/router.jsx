ec860d1f45deffda3ac86fb6a01551b75c614ce1
nguyen <tovannguyen12354@gmail.com>
2023-10-24 19:14:13 +0700

    login

--- src/router.jsx
+++ src/router.jsx
@@ -1,10 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom';
 import LayOut from './components/Layout';
+import Login from './formlogins/Login';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <LayOut />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
   },
 ]);
 
