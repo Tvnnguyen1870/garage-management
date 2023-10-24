524c4a1c8f0f1550a10cbf1ddef99cce4b347e4d
nguyen <tovannguyen12354@gmail.com>
2023-10-24 19:10:50 +0700

    set up layout

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,20 +1,7 @@
-import { theme } from 'antd';
 const Content = () => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
   return (
     <div>
-      <div
-        style={{
-          margin: '24px 16px',
-          padding: 24,
-          minHeight: 280,
-          background: colorBgContainer,
-        }}  
-      >
-        Content
-      </div>
+      <h1>content</h1>
     </div>
   );
 };
