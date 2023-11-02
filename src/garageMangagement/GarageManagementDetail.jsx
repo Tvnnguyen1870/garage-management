17812fb3a27ceee9cc0d79cee10eee5fee2eb5c7
tvnguyen2k <86392235+Tvnnguyen1870@users.noreply.github.com>
2023-11-02 22:14:25 +0700

    up code 2/11

--- src/garageMangagement/GarageManagementDetail.jsx
+++ src/garageMangagement/GarageManagementDetail.jsx
@@ -1,22 +1,57 @@
-import { Button, Col, Row } from 'antd';
+import { Breadcrumb, Button, Col, Row } from 'antd';
+import creategarage from '../assets/styles/creategarage.css';
 
 const GarageManagementDetail = () => {
   return (
-    <div>
-      <span>All garage ? gara 1</span>
-      <div>
+    <div className="garage-create-management">
+      <Breadcrumb
+        style={{
+          fontSize: 22,
+        }}
+        separator=">"
+        items={[
+          {
+            title: 'All garages',
+          },
+          {
+            title: 'Gara 1',
+          },
+        ]}
+      />
+      <div className="garage-create ">
         <Row>
-          <Col span={8}>col</Col>
-          <Col span={8}>col</Col>
-          <Col span={8}>col</Col>
+          <Col span={8}>
+            <span>Name</span>
+            <p>Nguyen</p>
+          </Col>
+          <Col span={8}>
+            <span>Email</span>
+            <p>tvn@gmail.com</p>
+          </Col>
+          <Col span={8}>
+            <span>Phone Number</span>
+            <p>023503205</p>
+          </Col>
         </Row>
         <Row>
-          <Col span={8}>col</Col>
-          <Col span={8}>col</Col>
-          <Col span={8}>col</Col>
+          <Col span={8}>
+            <span>Address</span>
+            <p>Ha Noi</p>
+          </Col>
+          <Col span={8}>
+            <span>Open time</span>
+            <p>9:00</p>
+          </Col>
+          <Col span={8}>
+            <span>Close time</span>
+            <p>22:00</p>
+          </Col>
         </Row>
         <Row>
-          <Col span={8}>col</Col>
+          <Col span={8}>
+            <span>Status</span>
+            <p>Active</p>
+          </Col>
         </Row>
         <Row>
           <Col span={8}>col</Col>
