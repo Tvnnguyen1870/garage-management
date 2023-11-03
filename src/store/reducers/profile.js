69c7668443a25d0348ea5a130d47437d1a724412
tvnguyen2k <86392235+Tvnnguyen1870@users.noreply.github.com>
2023-11-03 14:11:37 +0700

    123

--- src/store/reducers/profile.js
+++ src/store/reducers/profile.js
@@ -5,7 +5,7 @@ const initialState = {
     profile: null,
 }
 
-const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFlYjJjNzAxLTc4MWYtNDQyZS1hODQyLTc3ZDdlZTIxZmJiMCIsImVtYWlsIjoibmhvbTFAZ3JyLmxhIiwiZnVsbE5hbWUiOiJOaG9tIDEiLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2OTg4MjcxNzQsImV4cCI6MTY5ODg2MzE3NH0.0i5J8Nc5ma9WDjDO0uCGvYMQCLXuJlUgPAeMW-qb9MY';
+const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjFlYjJjNzAxLTc4MWYtNDQyZS1hODQyLTc3ZDdlZTIxZmJiMCIsImVtYWlsIjoibmhvbTFAZ3JyLmxhIiwiZnVsbE5hbWUiOiJuZ3V5ZW4iLCJyb2xlIjoiQURNSU4iLCJpYXQiOjE2OTg5OTUyNDcsImV4cCI6MTY5OTAzMTI0N30._7TN2s2qz8EYs6B35fp94qNj9aiLFPy7t8A4uhRrsg0';
 localStorage.setItem('accessToken', token)
 
 export const getProfile = createAsyncThunk(
